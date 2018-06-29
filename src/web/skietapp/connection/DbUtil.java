53a5965c3c8f57c26d87768736315e56d985bb1f
Skiecik <skieck@skietcode.hub.pl>
2018-06-29 10:12:53 +0200

    connection via context and DbUtil class

--- src/web/skietapp/connection/DbUtil.java
+++ src/web/skietapp/connection/DbUtil.java
@@ -1,5 +1,29 @@
 package web.skietapp.connection;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class DbUtil {
 
+	private static DataSource ds;
+	public static Connection getConn() throws SQLException {
+		return getInstance().getConnection();
+	}
+	
+	private static DataSource getInstance() {
+		if (ds == null) {
+			try {
+				Context ctx = new InitialContext();
+				ds = (DataSource)ctx.lookup("java:comp/env/jdbc/programmers_school");
+			} catch (NamingException e) {
+				e.printStackTrace();
+			}
+		}
+		return ds;
+	}
 }
